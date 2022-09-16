2a0cbc4af273c48d9b05ccba8d49cec9d0941022
msksingh <60178365+msksingh@users.noreply.github.com>
2022-09-16 13:38:16 +0200

    Update main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -1,10 +1,3 @@
-variable "instance_name" {}
-variable "instance_zone" {}
-variable "instance_type" {
-  default = "n1-standard-1"
-  }
-variable "instance_network" {}
-
 resource "google_compute_instance" "vm_instance" {
   name         = "${var.instance_name}"
   zone         = "${var.instance_zone}"
